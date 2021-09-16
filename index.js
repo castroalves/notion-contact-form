f140adcaace0e485d3ef323bd78964bd4a167ea1
Eduardo de Castro Alves <castro-alves@nextbike.com>
2021-09-16 12:01:44 +0200

    Add CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const { getContacts, addContact } = require("./services/notion");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
